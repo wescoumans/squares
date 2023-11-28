9df1e79bf870df013b65098476857f5919ca89f4
wescoumans <144227887+wescoumans@users.noreply.github.com>
2023-11-28 12:20:39 +0100

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,51 @@
+function setup () {
+    createCanvas(500,500);
+
+}
+
+function draw() {
+background(240);
+
+
+// Rect boven
+noStroke()
+fill(255,0,0);
+rect(50,150,50,50);
+
+fill(255,255,0);
+rect(150,150,50,50);
+
+fill(0,255,255);
+rect(250,150,50,50);
+
+fill(0,255,0);
+rect(350,150,50,50);
+
+
+
+
+
+//Rect onder
+stroke(0)
+fill(255);
+rect(50,300,50,50);
+
+fill(200);
+rect(150,300,50,50);
+
+
+fill(150);
+rect(250,300,50,50);
+
+fill(100);
+rect(350,300,50,50);
+
+
+
+
+
+
+
+
+
+}
